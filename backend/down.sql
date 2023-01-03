35a8a81c5d26cf61090340dc88d465b6f8bdcfed
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-01-03 20:37:49 +0100

    Remove misleading comments from `down.sql`

--- backend/down.sql
+++ backend/down.sql
@@ -1,22 +1,7 @@
 BEGIN;
---
--- Create model Employee
---
 DROP TABLE "hr_employee";
---
--- Create model Team
---
 DROP TABLE "hr_team";
---
--- Create model Contract
---
 DROP TABLE "hr_contract";
---
--- Create model BasicInfo
---
 DROP TABLE "hr_basicinfo";
---
--- Create model Address
---
 DROP TABLE "hr_address";
 COMMIT;
